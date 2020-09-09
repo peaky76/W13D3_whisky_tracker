deac022532d27e784c6bb157ba38c8fc756b9388
peaky76 <robertjamespeacock@gmail.com>
2020-09-09 21:30:49 +0100

    initial commit

---
+++ 4_lab_and_homework_start/WhiskyTracker/src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -0,0 +1,15 @@
+package com.codeclan.example.WhiskyTracker.repositories;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
+
+    List<Whisky> findByYear(int year);
+    List<Whisky> findByDistilleryNameIgnoreCaseAndAge(String distilleryName, int age);
+    List<Whisky> findByDistilleryRegionIgnoreCase(String region);
+
+}
